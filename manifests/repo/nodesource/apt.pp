fc88969d70703dc27ca758954ddff45565b789cf
Paul Talbot <prolixalias@gmail.com>
2022-11-05 14:38:23 -0400

    tracking down never-ending test

--- manifests/repo/nodesource/apt.pp
+++ manifests/repo/nodesource/apt.pp
@@ -26,30 +26,31 @@ class nodejs::repo::nodesource::apt (
   Optional[String] $pin = undef,
   Optional[String] $release = undef,
 ) {
-  include apt
+  # include apt
 
-  if ($ensure != 'absent') {
-    apt::source { 'nodesource':
-      include  => {
-        'src' => $enable_src,
-      },
-      key      => {
-        'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
-        'source' => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
-      },
-      location => "https://deb.nodesource.com/node_${url_suffix}",
-      pin      => $pin,
-      release  => $release,
-      repos    => 'main',
+  case $ensure {
+    'absent': {
+      apt::source { 'nodesource':
+        ensure => 'absent',
+      }
     }
+    default: {
+      apt::source { 'nodesource':
+        include  => {
+          'src' => $enable_src,
+        },
+        key      => {
+          'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
+          'source' => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+        },
+        location => "https://deb.nodesource.com/node_${url_suffix}",
+        pin      => $pin,
+        release  => $release,
+        repos    => 'main',
+      }
 
-    Apt::Source['nodesource'] -> Package<| tag == 'nodesource_repo' |>
-    Class['Apt::Update'] -> Package<| tag == 'nodesource_repo' |>
-  }
-
-  else {
-    apt::source { 'nodesource':
-      ensure => 'absent',
+      Apt::Source['nodesource'] -> Package<| tag == 'nodesource_repo' |>
+      Class['Apt::Update'] -> Package<| tag == 'nodesource_repo' |>
     }
   }
 }
